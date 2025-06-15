eea740edfd890b9dc52f53bc2d396eef444ed4d8
sudojustin <120149080+sudojustin@users.noreply.github.com>
2025-06-15 09:16:00 -0700

    Added get file content function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -28,3 +28,26 @@ def get_files_info(working_directory, directory=None):
 
     except Exception as e:
         return f'Error: {e}'
+
+
+def get_file_content(working_directory, file_path):
+    MAX_CHARS = 10000
+
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+    if not os.path.isfile(abs_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+
+    try:
+        with open(abs_file_path, 'r') as f:
+            file_content_string = f.read(MAX_CHARS)
+    except Exception as e:
+        return f'Error: {e}'
+
+    if len(file_content_string) > 10000:
+        return file_content_string + ' truncated at 10000 characters'
+    return file_content_string
--- tests.py
+++ tests.py
@@ -1,26 +1,38 @@
 import unittest
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_files_info, get_file_content
 
 class TestGetFilesInfo(unittest.TestCase):
-    def test_get_info_current_dir(self):
-        result = get_files_info('calculator', '.')
-        print('Result for current directory:')
-        print(f'{result}\n')
+    # def test_get_info_current_dir(self):
+    #     result = get_files_info('calculator', '.')
+    #     print('Result for current directory:')
+    #     print(f'{result}\n')
+    #
+    # def test_get_info_pkg(self):
+    #     result = get_files_info('calculator', 'pkg')
+    #     print('Result for "pkg" directory:')
+    #     print(f'{result}\n')
+    #
+    # def test_get_info_bin(self):
+    #     result = get_files_info('calculator', '/bin')
+    #     print('Result for "/bin" directory:')
+    #     print(f'{result}\n')
+    #
+    # def test_get_info_parent(self):
+    #     result = get_files_info('calculator', '../')
+    #     print('Result for "../" directory:')
+    #     print(f'{result}\n')
 
-    def test_get_info_pkg(self):
-        result = get_files_info('calculator', 'pkg')
-        print('Result for "pkg" directory:')
-        print(f'{result}\n')
+    def test_get_file_content_main(self):
+        result = get_file_content('calculator', 'main.py')
+        print(result)
 
-    def test_get_info_bin(self):
-        result = get_files_info('calculator', '/bin')
-        print('Result for "/bin" directory:')
-        print(f'{result}\n')
+    def test_get_file_content_pkg(self):
+        result = get_file_content('calculator', 'pkg/calculator.py')
+        print(result)
 
-    def test_get_info_parent(self):
-        result = get_files_info('calculator', '../')
-        print('Result for "../" directory:')
-        print(f'{result}\n')
+    def test_get_file_content_bin(self):
+        result = get_file_content('calculator', '/bin/cat')
+        print(result)
 
 
 if __name__ == '__main__':
