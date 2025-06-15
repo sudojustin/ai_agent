c92d9045cc1043020e969502c308af9be80b9483
sudojustin <120149080+sudojustin@users.noreply.github.com>
2025-06-15 12:05:11 -0700

    Added file writing

--- tests.py
+++ tests.py
@@ -1,39 +1,18 @@
 import unittest
-from functions.get_files_info import get_files_info, get_file_content
+from functions.get_files_info import write_file
 
 class TestGetFilesInfo(unittest.TestCase):
-    # def test_get_info_current_dir(self):
-    #     result = get_files_info('calculator', '.')
-    #     print('Result for current directory:')
-    #     print(f'{result}\n')
-    #
-    # def test_get_info_pkg(self):
-    #     result = get_files_info('calculator', 'pkg')
-    #     print('Result for "pkg" directory:')
-    #     print(f'{result}\n')
-    #
-    # def test_get_info_bin(self):
-    #     result = get_files_info('calculator', '/bin')
-    #     print('Result for "/bin" directory:')
-    #     print(f'{result}\n')
-    #
-    # def test_get_info_parent(self):
-    #     result = get_files_info('calculator', '../')
-    #     print('Result for "../" directory:')
-    #     print(f'{result}\n')
-
-    def test_get_file_content_main(self):
-        result = get_file_content('calculator', 'main.py')
+    def test_write_file_lorem(self):
+        result = write_file('calculator', 'lorem.txt', 'wait, this isn\'t lorem ipsum')
         print(result)
 
-    def test_get_file_content_pkg(self):
-        result = get_file_content('calculator', 'pkg/calculator.py')
+    def test_write_file_pkg(self):
+        result = write_file('calculator', 'pkg/morelorem.txt', 'lorem ipsum dolor sit amet')
         print(result)
 
-    def test_get_file_content_bin(self):
-        result = get_file_content('calculator', '/bin/cat')
+    def test_write_file_tmp(self):
+        result = write_file('calculator', '/tmp/temp.txt', 'this should not be allowed')
         print(result)
-
 
 if __name__ == '__main__':
     unittest.main()
